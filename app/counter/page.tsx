c2053003aa08daedd892a35327c352a6388d9a1c
aGoodMan999 <21521808@gm.uit.edu.vn>
2024-10-03 00:55:30 +0700

    Setup: add use store example

---
+++ app/counter/page.tsx
@@ -0,0 +1,27 @@
+'use client'
+import { useCounterStore } from "@/core/stores/CounterStore/CounterStore";
+import { Button, Card, CardContent, Grid2 } from "@mui/material";
+
+const Counter = () => {
+    const store = useCounterStore();
+    return (
+        <>
+            <div className="flex justify-center h-screen items-center">
+                <Card variant="outlined">
+                    <CardContent>
+                        <Grid2 spacing={2} container>
+                            <Grid2 className="flex justify-center" size={12}>
+                                <h1 className="text-5xl">{store.count}</h1>
+                            </Grid2>
+                            <Grid2 className="flex justify-center" size={12}>
+                                <Button variant="contained" color="success" onClick={store.inc}>Increase</Button>
+                            </Grid2>
+                        </Grid2>
+                    </CardContent>
+                </Card>
+            </div>
+        </>
+    )
+};
+
+export default Counter;
