dd63b38c1899e01a4a0fa1f593e9b6deca94b483
aGoodMan999 <21521808@gm.uit.edu.vn>
2024-10-26 22:07:21 +0700

    Add app logo

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,13 +1,17 @@
 import { Avatar, Button, Card, CardContent, Divider, Grid2, Link, TextField, Typography } from "@mui/material";
 import blue from "@mui/material/colors/blue";
+import Image from "next/image";
 
 const LoginPage: React.FC = () => {
     return (
-        <Grid2 sx={{ height: "100vh" }} container justifyContent="center" alignItems={"center"}>
-            <Card variant="outlined" className="w-[500px] h-[600px]" sx={{ boxShadow: 3 }}>
+        <Grid2 direction={"column"} container justifyContent="center" alignItems={"center"}>
+            <div className="w-full">
+                <Image src="/jobfinder-logo.png" width={180} height={180} alt="JobFinder logo"></Image>
+            </div>
+            <Card variant="outlined" className="w-[450px] h-[600px]" sx={{ boxShadow: 3 }}>
                 <CardContent>
                     <Grid2 rowSpacing={2} container direction="column" justifyContent={"center"}>
-                        <Typography variant="h5" textTransform="uppercase" fontWeight="bold">Login</Typography>
+                        <Typography variant="h5" textTransform="uppercase" fontWeight="bold">Sign in</Typography>
 
                         <Divider />
 
@@ -25,14 +29,18 @@ const LoginPage: React.FC = () => {
                         <Typography textAlign={"center"}>Hoặc</Typography>
 
                         <Grid2 columnGap={5} justifyContent={"center"} alignItems={"center"} container>
-                            <Button className="rounded-full">
-                                <Avatar src='/google-logo.png'></Avatar>
+                            <Button color="inherit" className="w-full justify-start p-3" variant="outlined">
+                                <Avatar className="w-[26px] h-[26px] mr-3" variant="square" src='/google-logo.png'></Avatar>
+                                <Typography textTransform={'none'}>Countinue with Google</Typography>
                             </Button>
-                            <Button className="rounded-full">
-                                <Avatar src='/facebook-logo.png'></Avatar>
+                            <Button color="inherit" className="w-full justify-start p-3" variant="outlined">
+                                <Avatar className="w-[26px] h-[26px] mr-3" variant="square" src='/facebook-logo.png'></Avatar>
+                                <Typography textTransform={'none'}>Countinue with Facebook</Typography>
+
                             </Button>
-                            <Button className="rounded-full">
-                                <Avatar src='/linkedin-logo.png'></Avatar>
+                            <Button color="inherit" className="w-full justify-start p-3" variant="outlined">
+                                <Avatar className="w-[26px] h-[26px] mr-3" variant="square" src='/linkedin-logo.png'></Avatar>
+                                <Typography textTransform={'none'}>Countinue with LinkedIn</Typography>
                             </Button>
                         </Grid2>
                     </Grid2>
