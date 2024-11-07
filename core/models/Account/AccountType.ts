80fd9e25e128810e9524f0e9115b4d83f3693c31
aGoodMan999 <21521808@gm.uit.edu.vn>
2024-11-07 23:30:28 +0700

    Add AccountType, GetAccountByJwt for AccountStore

---
+++ core/models/Account/AccountType.ts
@@ -0,0 +1,4 @@
+export enum AccountType {
+    User = 0,
+    Company = 1,
+}
--- core/stores/AccountStore/AccountStore.ts
+++ core/stores/AccountStore/AccountStore.ts
@@ -1,20 +1,51 @@
 import {create} from "zustand";
-import {Account} from "@/models";
+import {Account, CompanyAccount, UserAccount} from "@/models";
 import {AccountService} from "@/services/AccountService";
+import {AccountType} from "@/models/Account/AccountType";
 
 
 type AccountStore = {
     account: Account
+    accountType: AccountType | null
     login: (username: string, password: string) => Promise<void>
-
+    loadAccountByJwt: () => Promise<void>
 };
 
 
 export const useAccountStore = create<AccountStore>()((set, getState) => ({
     account: null,
+    accountType: null,
     login: async (username: string, password: string) => {
-        const account = await AccountService.login(username, password);
-        set(() => ({ account: account}));
+        await AccountService.login(username, password);
+    },
+    loadAccountByJwt: async () => {
+        const account = await AccountService.getAccountByCookie();
+        if(isUserAccount(account)) {
+            set(() => ({accountType: AccountType.User}))
+        } else if (isCompanyAccount(account)) {
+            set(() => ({accountType: AccountType.Company}))
+        } else {
+            set(() => ({accountType: null}))
+        }
+        set(() => ({account: account}));
     }
 }));
+
+const isUserAccount = (account: Account): account is UserAccount => {
+    return (
+        account !== null &&
+        'firstName' in account &&
+        'lastName' in account &&
+        'dateOfBirth' in account
+    );
+}
+
+const isCompanyAccount = (account: Account): account is CompanyAccount => {
+    return (
+        account !== null &&
+        'name' in account &&
+        'emailContact' in account &&
+        'phoneContact' in account
+    );
+}
 
