a4de51a33523bc05b39c2bbcf51e998f02c76e4c
aGoodMan999 <21521808@gm.uit.edu.vn>
2024-11-07 23:30:53 +0700

    update: Add example for AccountStore

--- app/login/user-profile/page.tsx
+++ app/login/user-profile/page.tsx
@@ -1,12 +1,21 @@
 "use client"
 import {useAccountStore} from "@/stores/AccountStore/AccountStore";
-import {UserAccount} from "@/models";
+import {CompanyAccount, UserAccount} from "@/models";
+import React from "react";
+import {AccountType} from "@/models/Account/AccountType";
 
 const UserProfilePage:React.FC  = () =>{
     const accountStore = useAccountStore();
+    React.useEffect(() => {
+        accountStore.loadAccountByJwt();
+    }, [])
+   React.useEffect(() => {
+       console.log()
+   }, [accountStore.account])
     return (
         <div>
-            {(accountStore.account as UserAccount).firstName}
+            {accountStore.accountType == AccountType.User && (accountStore.account as UserAccount)?.firstName}
+            {accountStore.accountType == AccountType.Company && (accountStore.account as CompanyAccount)?.name}
         </div>
     )
 };
