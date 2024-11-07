dacc44d0b47cb22d5a33d4a560f43ee443477661
aGoodMan999 <21521808@gm.uit.edu.vn>
2024-11-07 21:38:59 +0700

    update: add middleware for checking cookie

--- app/login/user-profile/page.tsx
+++ app/login/user-profile/page.tsx
@@ -1,13 +1,9 @@
 "use client"
 import {useAccountStore} from "@/stores/AccountStore/AccountStore";
 import {User} from "@/models";
-import {useEffect} from "react";
 
 const UserProfilePage:React.FC  = () =>{
     const accountStore = useAccountStore();
-    useEffect(() => {
-        console.log("account:",accountStore.account);
-    }, []);
     return (
         <div>
             {(accountStore.account as User).firstName}
---
+++ middleware.ts
@@ -0,0 +1,43 @@
+import {NextRequest, NextResponse} from 'next/server'
+import {http} from "@/services";
+import {Account} from "@/models";
+import {NextURL} from "next/dist/server/web/next-url";
+
+export const config = {
+    matcher: [
+        "/user-profile/:path",
+    ]
+}
+
+ export async function middleware(request: NextRequest) {
+    const token = request.cookies.get("auth_token")?.value;
+    if (!token) {
+        return NextResponse.redirect(new NextURL("/login", request.url));
+    }
+    const account = await getAccountByToken(token);
+    if (!account) {
+        return NextResponse.redirect(new NextURL("/login", request.url));
+    }
+}
+
+type ApiResult<T> = {
+    succeeded: boolean,
+    result: T,
+    error: string[]
+}
+
+const getAccountByToken = async (token:string) => {
+    try {
+        const url = "/Account/GetAccountByJwt";
+        const res = await http().get<ApiResult<Account>>(url, {
+            headers: {
+                "Cookie": `auth_token=${token}`,
+            }
+        });
+        return res.data.result;
+    } catch (e) {
+        console.log("ERROR", e);
+    }
+}
+
+
