453ab9a4ec221dd2a95936fbb0b9a209d949ad53
aGoodMan999 <21521808@gm.uit.edu.vn>
2024-11-11 16:51:26 +0700

    raw ui application dialog

--- app/job-detail/[id]/page.tsx
+++ app/job-detail/[id]/page.tsx
@@ -1,17 +1,36 @@
 "use client";
-import { Box, Card, CardContent, Container, Grid2 } from '@mui/material';
+import {
+    Box,
+    Card,
+    CardContent,
+    Checkbox,
+    Container,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogTitle,
+    Divider,
+    FormControl,
+    FormControlLabel,
+    Grid2,
+    TextField,
+    Typography, Button
+} from '@mui/material';
+import { blue, grey, lightBlue } from "@mui/material/colors"
+import Image from "next/image";
 import { CompanyIntro, JobBanner, JobBreadcrumb, JobInfo, JobList } from '@/components';
 import React, { useEffect } from 'react'
 import { useJobStore } from '@/stores';
 import { useParams } from 'next/navigation';
 import { useMetadataStore } from '@/stores/MetadataStore';
+import { BookmarkBorder, Email, InsertDriveFile } from "@mui/icons-material";
+
 
 
 const JobDetail = () => {
     const { id } = useParams();
     const jobStore = useJobStore();
     const metadataStore = useMetadataStore();
-
     useEffect(() => {
         jobStore.loadJobById(id as string);
     }, [id]);
@@ -23,12 +42,71 @@ const JobDetail = () => {
     return (
         <Box
             className="flex flex-col">
+            <Dialog open={true}>
+                <DialogTitle>Bạn đang ứng tuyển cho <strong>{jobStore.job.company.name}</strong> với vị
+                    trí <strong>{jobStore.job.title}</strong>
+                    <Image alt={jobStore.job.company.name} width={100} height={100} src={jobStore.job.company.logo}></Image>
+                </DialogTitle>
+                <DialogContent>
+                    <Box sx={{ display: "flex" }}>
+                        <InsertDriveFile sx={{ marginRight: 1 }}></InsertDriveFile>
+                        <Typography sx={{ fontWeight: "bold" }}>
+                            Hồ sơ xin việc *
+                        </Typography>
+                    </Box>
+                    <Box sx={{ border: 1, borderColor: blue[500], borderStyle: "dashed", borderRadius: 1, display: "flex", justifyContent: "center", padding: 2, background: lightBlue[50], color: blue[500] }}>
+                        <InsertDriveFile sx={{ marginX: 1 }}></InsertDriveFile>
+                        <Typography sx={{ fontWeight: "bold" }}>
+                            Đăng tải hồ sơ của tôi
+                        </Typography>
+                    </Box>
+                    <Typography variant='body2' sx={{ color: grey[500] }} >
+                        Lưu ý : đảm bảo hồ sơ xin việc của bạn sử dụng ngôn ngữ trùng khớp với mô tả công việc (Ví dụ: viết CV bằng tiếng Anh nếu mô tả công việc bằng tiếng Anh) và đăng tải dưới dạng PDF dưới 5MB.
+                        Hồ sơ đã đăng tải sẽ được lưu lại cho lần nộp đơn sau.
+                    </Typography>
+                    <TextField required label='Số điện thoại' type="number" sx={{ marginTop: 1 }} slotProps={{
+                        input: {
+                            startAdornment:
+                                <Box sx={{ display: "inline-flex" }}>
+                                    <Typography>+84</Typography>
+                                    <Divider sx={{ opacity: 1, borderRightWidth: 2, marginX: 1 }} variant='fullWidth' orientation='vertical' flexItem></Divider>
+                                </Box>
+                        }
+                    }}></TextField>
+                    <Box display="flex">
+                        <Email sx={{ marginRight: 1 }}></Email>
+                        <Typography fontWeight={"bold"}>
+                            Thư xin việc
+                        </Typography>
+                    </Box>
+                    <FormControl fullWidth>
+                        <FormControlLabel label="Tôi đã có thư xin việc" control={<Checkbox></Checkbox>}>
+                        </FormControlLabel>
+                        <TextField label="Viết thư xin việc của bạn" rows={4} multiline fullWidth></TextField>
+                    </FormControl>
+                </DialogContent>
+                <DialogActions>
+                    <Box width={"100%"} display="flex" flexDirection="column" justifyContent="center">
+                        <Button sx={{ height: 50 }} fullWidth variant="contained" color="primary">
+                            ỨNG TUYỂN NGAY
+                        </Button>
+                        <Typography textAlign="center">Bạn chưa chuẩn bị hồ sơ?</Typography>
+                        <Typography textAlign={"center"}>Hãy lưu lại việc làm để ứng tuyển sau.</Typography>
+                        <Box display={"flex"} justifyContent={"center"} color={blue[500]}>
+                            <BookmarkBorder sx={{ marginRight: 1 }}></BookmarkBorder>
+                            <Typography textAlign={"center"} >
+                                Đánh dấu lưu lại vị trí công việc
+                            </Typography>
+                        </Box>
+                    </Box>
+                </DialogActions>
+            </Dialog>
             <Container
                 component="main"
                 maxWidth="lg"
                 className="flex-grow p-4">
                 <JobBreadcrumb
-                    currentPosition={`Tuyển dụng ${jobStore.job.title}`}>
+                    currentPosition={`${jobStore.job.title}`}>
                 </JobBreadcrumb>
             </Container>
             <Box className=" bg-colorPrimary">
@@ -80,7 +158,7 @@ const JobDetail = () => {
 
                 </Grid2>
             </Container>
-        </Box>
+        </Box >
 
     );
 }
