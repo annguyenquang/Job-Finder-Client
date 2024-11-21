868736474cbc40e5a69e7f7b443fd669b771bf4d
aGoodMan999 <21521808@gm.uit.edu.vn>
2024-11-21 17:31:00 +0700

    chore: fix file naming error

--- components/profile/BasicInfo/BasicInfoSection.tsx
+++ components/profile/BasicInfo/BasicInfoSection.tsx
@@ -1,14 +1,14 @@
 import { UserAccount } from "@/models"
 import Grid2 from "@mui/material/Grid2"
-import RightBasicInfo from "./RightBasicInfo"
 import LeftBasicInfo from "./LeftBasicInfo"
+import RightBasicInfo from "./RightBasicInfo"
 
 export const BasicInfoSection: React.FC<{ user: UserAccount | null }> = ({ user }) => {
 
     return (
         <Grid2 container>
-            <RightBasicInfo user={user}></RightBasicInfo>
-            <LeftBasicInfo></LeftBasicInfo>
+            <LeftBasicInfo user={user}></LeftBasicInfo>
+            <RightBasicInfo></RightBasicInfo>
         </Grid2>
     )
 }
--- components/profile/BasicInfo/RightBasicInfo.tsx
+++ components/profile/BasicInfo/RightBasicInfo.tsx
@@ -1,39 +1,20 @@
-import { UserAccount } from "@/models";
-import Edit from "@mui/icons-material/Edit";
-import blue from "@mui/material/colors/blue";
 import Grid2 from "@mui/material/Grid2";
 import Stack from "@mui/material/Stack";
-import Typography from "@mui/material/Typography";
-import Image from 'next/image'
-import InfoWithLabel from "./InfoWithLabel";
+import ProfileAction from "./ProfileAction";
+import Button from "@mui/material/Button";
+import NavigateNext from "@mui/icons-material/NavigateNext";
+import QuickAction from "./QuickAction";
 
-const RightBasicInfo: React.FC<{ user: UserAccount | null }> = ({ user }) => {
+const RightBasicInfo: React.FC = () => {
     return (
-        <Grid2 container flexDirection={'row'} size={9}>
-            <Image width={150} height={150}
-                src={'https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcQupkN1PYtfnG4h8hndNBuk1riMuW2Z8Uf-buFTioHubkTVUJ-9NES18ItN9N1zIra3OJglhvUrOH4WEJc'}
-                alt={'Avatar'}></Image>
-            <Stack direction={'column'}>
-                <Stack spacing={2} direction={'row'}>
-                    <Typography fontWeight={'bold'} variant={'h5'}>{user?.firstName + ' ' + user?.lastName}</Typography>
-                    <Stack color={blue[500]} direction={'row'}>
-                        <Edit fontSize={'small'}></Edit>
-                        <Typography variant={'body1'}>Chỉnh sửa thông tin cơ bản</Typography>
-                    </Stack>
-                </Stack>
-                <Grid2 container>
-                    <Grid2 size={6}>
-                        <InfoWithLabel label={'SỐ ĐIỆN THOẠI'} value={user?.phone ? user.phone : "_"}></InfoWithLabel>
-                        <InfoWithLabel label={'NGÀY SINH'} value={user?.dateOfBirth ? user.dateOfBirth.toLocaleDateString() : ""}></InfoWithLabel>
-                    </Grid2>
-                    <Grid2 size={6}>
-                        <InfoWithLabel label={'EMAIL'} value={user?.email ? user.email : "_"}></InfoWithLabel>
-                        <InfoWithLabel label={'Giới tính'} value="_"></InfoWithLabel>
-                    </Grid2>
-                </Grid2>
+        <Grid2 size={3}>
+            <Stack>
+                <ProfileAction label={'Quản lý tài khoản'}>
+                    <Button endIcon={<NavigateNext></NavigateNext>}>Không tìm kiếm cơ hội mới</Button>
+                </ProfileAction>
+                <QuickAction></QuickAction>
             </Stack>
         </Grid2>
     )
 }
-
 export default RightBasicInfo;
