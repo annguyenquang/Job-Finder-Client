6bdec542c6200bb5a1d736d67bfc0f0907e7a58f
aGoodMan999 <21521808@gm.uit.edu.vn>
2024-12-10 21:26:00 +0700

    hot fix next image source in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,21 +1,28 @@
 // next.config.js
 module.exports = {
-    images : {
-        remotePatterns: [{
-            protocol: 'https',
-            hostname: 'images-platform.99static.com',
-            port: '',
-            pathname: "**"
-        },
-            {
-                protocol: 'https',
-                hostname: 'encrypted-tbn0.gstatic.com',
-                port: '',
-                pathname: "**"
-            }
-        ],
-    },
-    typescript: {
-        ignoreBuildErrors: false, // Don't allow Next.js to run with TypeScript errors
-    },
-};
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images-platform.99static.com',
+        port: '',
+        pathname: '**'
+      },
+      {
+        protocol: 'https',
+        hostname: 'encrypted-tbn0.gstatic.com',
+        port: '',
+        pathname: '**'
+      },
+      {
+        protocol: 'https',
+        hostname: 'dummyimage.com',
+        port: '',
+        pathname: '**'
+      }
+    ]
+  },
+  typescript: {
+    ignoreBuildErrors: false // Don't allow Next.js to run with TypeScript errors
+  }
+}
