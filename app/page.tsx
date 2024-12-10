c69a89032931bc9c55ab9e47791d997c0ebeae11
aGoodMan999 <21521808@gm.uit.edu.vn>
2024-12-11 00:25:40 +0700

    clean

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,6 @@
 'use client'
-import { Button, Container, Typography } from '@mui/material'
-import { Navbar } from '@/components'
-import { useRouter } from 'next/navigation'
 
 const Home = () => {
-  const router = useRouter()
-
   return <div>Developing</div>
 }
 
