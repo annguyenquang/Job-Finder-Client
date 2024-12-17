bfe46d9a31e4c0d20c2588d2ecc28f0d43742a47
aGoodMan999 <21521808@gm.uit.edu.vn>
2024-12-17 15:43:54 +0700

    saving

--- app/(protected)/company/dashboard/page.tsx
+++ app/(protected)/company/dashboard/page.tsx
@@ -1,10 +1,84 @@
-import { CheckCircle } from '@mui/icons-material'
-import { Box, Button, ButtonGroup, Divider, Stack, Typography } from '@mui/material'
+import { CheckCircle, SwapVert } from '@mui/icons-material'
+import Search from '@mui/icons-material/Search'
+import { TabContext, TabList, TabPanel } from '@mui/lab'
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Divider,
+  InputAdornment,
+  Stack,
+  Tab,
+  Tabs,
+  TextField,
+  Typography
+} from '@mui/material'
 import { green } from '@mui/material/colors'
 import Image from 'next/image'
 
 const CompanyPage: React.FC = () => {
-  return <BasicCompanyInfo />
+  return (
+    <Stack>
+      <Box padding={1}>
+        <BasicCompanyInfo />
+      </Box>
+      <Box padding={1}>
+        <JobList />
+      </Box>
+    </Stack>
+  )
+}
+
+const JobList: React.FC = () => {
+  return (
+    <Stack>
+      <Typography variant='h6'>Danh sách tin</Typography>
+      <Box position={'relative'}>
+        <TextField
+          size='small'
+          sx={{ width: '45%' }}
+          placeholder='Tìm kiếm công việc'
+          slotProps={{
+            input: {
+              startAdornment: (
+                <InputAdornment position='start'>
+                  <Search></Search>
+                </InputAdornment>
+              )
+            }
+          }}
+        />
+
+        <Button
+          variant='outlined'
+          sx={{ position: 'absolute', right: 0, fontWeight: 'bold' }}
+          startIcon={<SwapVert></SwapVert>}
+        >
+          Sắp xếp theo:{' '}
+        </Button>
+
+        <TabContext value={2}>
+          <TabList>
+            <Tab
+              label='Tất cả tin'
+              value='0'
+            />
+            <Tab
+              label='Tin đang hoạt động'
+              value='1'
+            />
+            <Tab
+              label='Tin hết hạn'
+              value='2'
+            />
+          </TabList>
+          <TabPanel value={0}>ITem1</TabPanel>
+          <TabPanel value={1}>ITem2</TabPanel>
+          <TabPanel value={2}>ITem3</TabPanel>
+        </TabContext>
+      </Box>
+    </Stack>
+  )
 }
 
 const BasicCompanyInfo: React.FC = () => {
@@ -28,7 +102,12 @@ const BasicCompanyInfo: React.FC = () => {
         <Divider orientation={'vertical'} />
 
         <Stack direction={'column'}>
-          <Typography variant='h6'>AN</Typography>
+          <Typography
+            variant='h5'
+            fontWeight={'bold'}
+          >
+            AN
+          </Typography>
           <Stack
             direction={'row'}
             spacing={1}
