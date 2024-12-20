c2efac304717ae2a28504119806a2ae5f699d637
aGoodMan999 <21521808@gm.uit.edu.vn>
2024-12-20 13:02:35 +0700

    chore: refactor eslint error

--- core/models/CompanyModel/Company.ts
+++ core/models/CompanyModel/Company.ts
@@ -1,19 +1,19 @@
 export type Company = {
-    id: string; // Mã công ty
-    name: string; // Tên công ty
-    emailContact: string; // Email liên hệ 
-    phoneContact: string; // Số điện thoại liên hệ 
-    employeeCount: number; // Số lượng nhân viên
-    slug: string; // Tên mã hóa
-    address: string; // Địa chỉ công ty
-    website: string; // Website 
-    industry: string; // Ngành nghề 
-    createdAt?: Date; // Ngày tạo
-    updatedAt?: Date; // Ngày cập nhật
-    createdBy?: string; // Người tạo
-    updatedBy?: string; // Người cập nhật
-    districtId: number; // ID quận 
-    provinceId: number; // ID tỉnh 
-    description: string; // Mô tả 
-    logo: string; // Logo 
+  id: string // Mã công ty
+  name: string // Tên công ty
+  emailContact: string // Email liên hệ
+  phoneContact: string // Số điện thoại liên hệ
+  employeeCount: number // Số lượng nhân viên
+  slug: string // Tên mã hóa
+  address: string // Địa chỉ công ty
+  website: string // Website
+  industry: string // Ngành nghề
+  createdAt?: Date // Ngày tạo
+  updatedAt?: Date // Ngày cập nhật
+  createdBy?: string // Người tạo
+  updatedBy?: string // Người cập nhật
+  districtId: number // ID quận
+  provinceId: number // ID tỉnh
+  description: string // Mô tả
+  logo: string // Logo
 }
